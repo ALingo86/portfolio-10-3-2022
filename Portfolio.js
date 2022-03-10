f36990eb636fe7cae98bfabe43c0a349f8a39c80
Alingo86 <andrew.j.lingo@gmail.com>
2022-03-10 15:25:36 -0500

    init

---
+++ Portfolio.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('.projects-btn');
+let el = document.querySelector('.projects-header');
+
+btn.addEventListener('click', function() {
+    el.scrollIntoView(true);
+});
